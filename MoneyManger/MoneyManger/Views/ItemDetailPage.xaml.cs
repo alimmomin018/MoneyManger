0c9238773230b9748e87e5157dadeaef25d156b4
Alim Momin <alim@webility.ca>
2022-04-01 12:47:36 -0400

    Initial commit

---
+++ MoneyManger/MoneyManger/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MoneyManger.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MoneyManger.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
