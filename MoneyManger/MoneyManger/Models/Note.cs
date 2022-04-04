e82fb7408c6c8906b32828f4c853db0b7ce4d654
Alim Momin <alim@webility.ca>
2022-04-04 09:39:35 -0400

    removed unnecessary files and added font icons, fonts and homepage

---
+++ MoneyManger/MoneyManger/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace MoneyManger.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int NoteId { get; set; }
+        public string Notes { get; set; }
+    }
+}
--- MoneyManger/MoneyManger/Models/Person.cs
+++ MoneyManger/MoneyManger/Models/Person.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 
 namespace MoneyManger.Models
 {
     public class Person
     {
+        [PrimaryKey, AutoIncrement]
         public string Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
         public List<Transaction> Transaction { get; set; }
+        [Ignore]
+        public string LastUpdated { get; set; }
+        [Ignore]
+        public decimal Total { get; set; }
     }
 }
