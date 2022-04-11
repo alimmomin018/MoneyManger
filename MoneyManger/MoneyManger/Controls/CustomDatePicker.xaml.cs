d44762dade231217f62eb35504a8a8e196b9249a
Alim Momin <alim@webility.ca>
2022-04-11 09:25:57 -0400

    added custom datepicker control

---
+++ MoneyManger/MoneyManger/Controls/CustomDatePicker.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyManger.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomDatePicker : Grid
+    {
+        public static BindableProperty CustomDateProperty = BindableProperty.Create(nameof(CustomDate), typeof(DateTime),
+                 typeof(CustomDatePicker), default(DateTime), BindingMode.TwoWay);
+
+        public static BindableProperty PlaceHolderProperty = BindableProperty.Create(nameof(PlaceHolder), typeof(string),
+                 typeof(CustomDatePicker), default(string), BindingMode.OneWay);
+
+        //private static void CustomDatePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        //{
+        //    var control = (CustomDatePicker)bindable;
+        //    control.edpDate.Date = (DateTime)newValue;
+        //}
+
+        //private static void PlaceHolderPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        //{
+        //    var control = (CustomDatePicker)bindable;
+        //    control.edpLabel.Text = newValue.ToString();
+        //}
+
+        public CustomDatePicker()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public DateTime CustomDate
+        {
+            get { return (DateTime)GetValue(CustomDateProperty); }
+            set
+            {
+                SetValue(CustomDateProperty, value);
+            }
+        }
+        
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set
+            {
+                SetValue(PlaceHolderProperty, value);
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            edpDate.Focus();
+        }
+    }
+}
