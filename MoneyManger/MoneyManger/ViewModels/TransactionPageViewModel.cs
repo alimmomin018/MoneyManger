ea8bf32de4dd3a483cd745d763d74d733381edfa
Alim Momin <alim@webility.ca>
2022-04-19 16:14:21 -0400

    added searchbar animation

--- MoneyManger/MoneyManger/ViewModels/TransactionPageViewModel.cs
+++ MoneyManger/MoneyManger/ViewModels/TransactionPageViewModel.cs
@@ -2,11 +2,7 @@
 using MoneyManger.Models;
 using MoneyManger.Views;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms;
